4dd03e4a02460cadc61eee5c78b3243c95ea499f
Anandha Krishnan Aji <45736365+anandhakrishnanaji@users.noreply.github.com>
2020-02-06 16:18:37 +0530

    Add files via upload

---
+++ po.py
@@ -0,0 +1,11 @@
+import requests
+with requests.Session() as s:
+    url = 'https://www.reddit.com/login/'
+    uurl='https://www.reddit.com/user/anandhakris'
+    payload={'loginUsername':'*type-your-user_id*','loginPassword':'*type-your-password*'}
+    post = s.post(url, data=payload)
+    r = s.get(uurl)
+    print(r.content)
+#To Run
+#python3 po.py > filename.html
+#firefox filename.html  
